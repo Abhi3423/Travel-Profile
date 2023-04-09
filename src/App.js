325d1100c21391705285fccba3fab908ab1b6032
Abhishek Yadav <abhishekaug2002@gmail.com>
2023-04-10 01:22:50 +0530

    title and scrollbar changed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
-import { Routes } from "./routes/AppRoute"
+import { Routes } from "./routes/AppRoute";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 const App = () => {
+
+  const location = useLocation();
+
+  const routeTitles = {
+    "/": "Respondent Travel profile",
+    "/mode-choice": "Mode choice",
+    "/another-route": "Another Route Title"
+  };
+
+  useEffect(() => {
+    document.title = routeTitles[location.pathname];
+  }, [location]);
+
   return (
     <div className="block box-border">
       <Routes />
